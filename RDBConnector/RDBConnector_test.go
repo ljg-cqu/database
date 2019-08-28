a4003498924242e2b1213b9e70c617ad243052e0
Zealy <ljg_cqu@126.com>
2019-08-28 19:05:12 +0800

    test

---
+++ RDBConnector/RDBConnector_test.go
@@ -0,0 +1,22 @@
+package RDBConnector
+
+import (
+	"testing"
+)
+
+func Test_CreateRDBConnector(t *testing.T) {
+	// Test happy path
+	t.Run("happy path", func(t *testing.T){
+		
+	})
+
+	// Test input errors
+	t.Run("input errors", func(t *testing.T){
+		
+	})
+
+	// Test dependency issues
+	t.Run("dependency issues", func(t *testing.T){
+		
+	})
+}
