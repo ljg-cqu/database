f1773ffc46f7068c8b1f82b374c3d0ef7e49b73b
Zealy <ljg_cqu@126.com>
2019-08-28 19:06:44 +0800

    test2

--- RDBConnector/RDBConnector.go
+++ RDBConnector/RDBConnector.go
@@ -1,1 +1,84 @@
 
+// RDBConnector package implements relational database connector.
+// Based on gorm library, with PostgreSQL supported as default.
+package RDBConnector
+
+import (
+	"time"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	// _ "github.com/jinzhu/gorm/dialects/sqlite"
+	// _ "github.com/jinzhu/gorm/dialects/mysql"
+	// _ "github.com/jinzhu/gorm/dialects/mssql"
+)
+
+// Default relational database configuration
+var defaultRDBConfig RDBConfig
+
+// Options for user-specified relational database configuration
+type RDBConfigOption func(config *RDBConfig) error
+
+// Data type for relational database configuration
+type RDBConfig struct {
+    DriverName string 
+    DSN string 
+    ConnMaxLifetime time.Duration
+    MaxIdleConns int
+    MaxOpenConns int
+}
+
+// Relational database connector that wraps *gorm.DB
+type RDBConnector struct {
+	config *RDBConfig
+	GormDB *gorm.DB
+}
+
+// Create relational database connector with configuration
+func CreateRDBConnector(options ...RDBConfigOption) *RDBConnector {
+	// Get relational database configuration
+    config := createRDBConfig(options...)
+
+    // Connect to relational database based on gorm library
+    db, err := gorm.Open(config.DriverName, config.DSN)
+    if err != nil {
+    	panic(err)
+	}
+
+	// Set connection parameters via generic database interface 
+	DB := db.DB()
+	DB.SetConnMaxLifetime(config.ConnMaxLifetime)
+	DB.SetMaxIdleConns(config.MaxIdleConns)
+	DB.SetMaxOpenConns(config.MaxOpenConns)
+
+    return &RDBConnector{config, db}
+}
+
+// Create relational database configuration according to user's options,
+// or return the default values in case of no options input
+func createRDBConfig(options ...RDBConfigOption) *RDBConfig {
+	if len(options) == 0 {
+		return &defaultRDBConfig
+	}
+
+	// Copy default coniguration parameters
+	userRDBConfig := defaultRDBConfig
+    for _, option := range options {
+    	if err := option(&userRDBConfig); err != nil {
+    		panic(err)
+		}
+	}
+    return &userRDBConfig
+}
+
+func init() {
+	// Set default relational database configuration
+	defaultRDBConfig = RDBConfig {
+		DriverName: "postgres",
+		// dbname default as postgres
+		DSN: "host=localhost port=5432 user=postgres password=QrfV2_Pg sslmode=disable",
+		ConnMaxLifetime: time.Second * 10,
+		MaxIdleConns: 100,
+		MaxOpenConns: 1000,
+	}
+}
--- RDBConnector/RDBConnector_test.go
+++ RDBConnector/RDBConnector_test.go
@@ -1,22 +1,92 @@
 package RDBConnector
 
 import (
+	"fmt"
+	"errors"
 	"testing"
+	"github.com/jinzhu/gorm"
 )
 
 func Test_CreateRDBConnector(t *testing.T) {
 	// Test happy path
 	t.Run("happy path", func(t *testing.T){
-		
+		t.Run("use default config", func(t *testing.T){
+			c := CreateRDBConnector()
+			if c.config.DriverName != "postgres" ||  c.GormDB == nil {
+				t.Error("Failed creating RDBConnector")
+				return
+			}
+			stats := c.GormDB.DB().Stats()
+			fmt.Printf("%#v", stats)
+
+			type Product struct {
+				gorm.Model
+				Name string
+			}
+			c.GormDB = c.GormDB.AutoMigrate(&Product{})
+			if c.GormDB.Error != nil {
+				t.Error(c.GormDB.Error)
+			}
+		})
+
+		t.Run("switch to another existing database",func(t *testing.T){
+			// testDB database should be created beforehand
+			newDSN := "host=localhost port=5432 user=postgres password=QrfV2_Pg sslmode=disable dbname=testDB"
+			switchDB := func(config *RDBConfig) error {
+				config.DSN = newDSN
+				return nil
+			}
+
+			c := CreateRDBConnector(switchDB)
+			if c.config.DSN != newDSN || c.GormDB == nil {
+				t.Error("Failed creating RDBConnector")
+				return
+			} 
+
+			type Product struct {
+				gorm.Model
+				Name string
+			}
+			c.GormDB = c.GormDB.CreateTable(&Product{})
+			if c.GormDB.Error != nil {
+				t.Error("Failed creating table. Err: ", c.GormDB.Error)
+			}
+		})
 	})
 
 	// Test input errors
 	t.Run("input errors", func(t *testing.T){
-		
+		t.Run("driver name error", func(t *testing.T){
+			driverError := func(config *RDBConfig) error {
+				config.DriverName = "wrongDriverName"
+				return nil
+			}
+			CreateRDBConnector(driverError)
+		})
+
+		t.Run("driver name error 2", func(t *testing.T){
+			driverError := func(config *RDBConfig) error {
+				return errors.New("Wrong driver name")
+			}
+			CreateRDBConnector(driverError)
+		})
 	})
 
 	// Test dependency issues
 	t.Run("dependency issues", func(t *testing.T){
-		
+		t.Run("database not ready", func(t *testing.T){
+			// testDB2 database does not be created beforehand
+			newDSN := "host=localhost port=5432 user=postgres password=QrfV2_Pg sslmode=disable dbname=testDB2"
+			switchDB := func(config *RDBConfig) error {
+				config.DSN = newDSN
+				return nil
+			}
+
+			c := CreateRDBConnector(switchDB)
+			if c.config.DSN != newDSN || c.GormDB == nil {
+				t.Error("Failed creating RDBConnector")
+				return
+			} 
+		})
 	})
 }
